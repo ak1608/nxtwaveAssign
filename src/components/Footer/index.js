02eebb581a73426e5aa7869dfe44ba62ac837247
ak1608 <48668400+ak1608@users.noreply.github.com>
2023-09-19 16:39:36 +0000

    footer done

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,50 @@
 
 import "./index.css";
-import { PiCopyrightFill } from "react-icons/pi";
+import { PiCopyrightFill,PiPhoneCall } from "react-icons/pi";
+
 
 
 const Footer =() =>(
     <>
     <div className="footerContainer">
         <div className="footertop">
-            <div></div>
+            <div className="topleft">
+                <div>
+                <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695120352/assignment/NIL_logo_Transparent_1_1_npdqms.png" alt="Italian Trulli" className="footerlogo"/>
 
-            <div className="serv">
-                <h1>Services</h1>
+
+                </div>
+                <p className="footerpara">Book doctor appointments online and get your ailments treated in no time with our expert medical support systems. Get rid of all your medical problem with our expert panel of doctors who guide you towards a healthier life.</p>
+                <div className="phone">
+                    <div>
+                        <p className="callno"> +91</p>
+                        
+                        <div class="vl"></div>
+
+                    </div>
+                    <div>
+                        <div className="callcontainer">
+                        <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695141013/assignment/call_1_mnia18.svg" alt="Italian Trulli" className="call"/>
+
+                        </div>
+                     
+                    
+
+                    </div>
+
+                    
+                    
+                    
+
+                </div>
+
+
+
+
+            </div>
+
+            <div  className="footerlis">
+                <h1 className="serv">Services</h1>
                 <ul className="lis">
                     <li>Appointments</li>
                     <li>Lab Tests</li>
@@ -18,8 +52,8 @@ const Footer =() =>(
                     <li>Doctor Support</li>
                 </ul>
             </div>
-            <div className="serv">
-                <h1>Legal</h1>
+            <div className="footerlis" >
+                <h1 className="serv">Legal</h1>
                 <ul className="lis">
                     <li>General info</li>
                     <li>Privacy policy</li>
@@ -28,8 +62,8 @@ const Footer =() =>(
                     <li>How it works</li>
                 </ul>
             </div>
-            <div className="serv">
-                <h1>Talk to us</h1>
+            <div  className="footerlis">
+                <h1 className="serv">Talk to us</h1>
                 <ul className="lis">
                     <li>support@notinline.com</li>
                     <li>Lab appointment@notinline.com</li>
