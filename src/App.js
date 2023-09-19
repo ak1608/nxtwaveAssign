a8936b14d765e32396bda869ce8926ca8fc650e4
ak1608 <48668400+ak1608@users.noreply.github.com>
2023-09-19 17:08:28 +0000

    accredation complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import Toplanding from './components/Toplanding';
 
 function App() {
   return (
     <>
     <Header />
+    <Toplanding />
     <Footer />
     
     
---
+++ src/components/Toplanding/index.js
@@ -0,0 +1,31 @@
+
+import "./index.css";
+
+
+
+const Toplanding =()=>(
+    <>
+    <div className="bgcontainer">
+
+    </div>
+    <div className="accred">
+        <div className="acContainer">
+        <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695142501/assignment/Rectangle_80_czfeaw.png" alt="Italian Trulli" className="toplogo" />
+            <p className="para">NABL Accredited Labs</p>
+        </div>
+        <div className="acContainer">
+        <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695142501/assignment/Rectangle_81_rirffm.png" />
+            <p className="para">Free Sample Collection</p>
+        </div>
+
+    </div>
+    
+    
+    
+    
+    
+    
+    </>
+)
+
+export default Toplanding;
