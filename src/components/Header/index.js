95a24e56a37f78d65d225583c074168d61cfe977
ak1608 <48668400+ak1608@users.noreply.github.com>
2023-09-19 12:27:35 +0000

    navbar done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,29 +9,45 @@ const Header = () => (
             <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695120414/assignment/Screenshot_2023-07-25_165513-removebg-preview_1_1_nfcp0u.png" alt="Italian Trulli" className="logoname" />
 
             </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav group1 ">
+                <li className="nav-item">
+                <a className="nav-link hometext active" aria-current="page" href="#">Home</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
+                <li className="nav-item">
+                <a className="nav-link healthtext active" aria-current="page" href="#">Health Conditions</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
+                <li className="nav-item">
+                <a className="nav-link labtext active" aria-current="page" href="#">Lab Tests</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
+                <li className="nav-item">
+                <a className="nav-link medicinestext active" aria-current="page" href="#">Medicines</a>
                 </li>
+                 
             </ul>
+            
+                <div>
+                    <button className="forpatients m-1">For Patients</button>
+                </div>
+                <div>
+                    <button className="forpatients m-1">For Hospitals</button>
+                </div>
+                <div className="btt">
+                    <button className="forpatients m-1">Play Store</button>
+                </div>
+            
+
             </div>
+
+            
+            
         </div>
     </nav>
-    <h1>hello</h1>
-    <h1>hello</h1>
+     
+     
 
     
     
