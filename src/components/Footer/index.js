1eb6d8f5891cb2b1792cc4a7f4a8f3b0bea2394a
ak1608 <48668400+ak1608@users.noreply.github.com>
2023-09-19 15:05:12 +0000

    serv in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,6 +6,49 @@ import { PiCopyrightFill } from "react-icons/pi";
 const Footer =() =>(
     <>
     <div className="footerContainer">
+        <div className="footertop">
+            <div></div>
+
+            <div className="serv">
+                <h1>Services</h1>
+                <ul className="lis">
+                    <li>Appointments</li>
+                    <li>Lab Tests</li>
+                    <li>A-Z Medicine</li>
+                    <li>Doctor Support</li>
+                </ul>
+            </div>
+            <div className="serv">
+                <h1>Legal</h1>
+                <ul className="lis">
+                    <li>General info</li>
+                    <li>Privacy policy</li>
+                    <li>A-Z Terms of services</li>
+                    <li>Consultation</li>
+                    <li>How it works</li>
+                </ul>
+            </div>
+            <div className="serv">
+                <h1>Talk to us</h1>
+                <ul className="lis">
+                    <li>support@notinline.com</li>
+                    <li>Lab appointment@notinline.com</li>
+                    <li>+91 12345 67899</li>
+                    <li>+91 97642 09752</li>
+                </ul>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+        </div>
 
 
 
