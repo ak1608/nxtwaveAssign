1e58517fa0be31e552fb8955b4072a77947e24ae
ak1608 <48668400+ak1608@users.noreply.github.com>
2023-09-19 14:50:37 +0000

    footer -copywright

---
+++ src/components/Footer/index.js
@@ -0,0 +1,27 @@
+
+import "./index.css";
+import { PiCopyrightFill } from "react-icons/pi";
+
+
+const Footer =() =>(
+    <>
+    <div className="footerContainer">
+
+
+
+        
+        <p  className="check">hrssssll</p>
+        <hr className="hrline"  />
+        <div className="copyright">
+            <PiCopyrightFill className="icon" />
+            <p className="copyText">2021-2023 All Rights Reserved</p>
+        </div>
+
+    </div>
+    
+ 
+    
+    </>
+)
+
+export default Footer
