5d6773927d2f4f844cff48582ae0765fa311caf8
ak1608 <48668400+ak1608@users.noreply.github.com>
2023-09-19 18:58:45 +0000

    form completed

--- src/components/Toplanding/index.js
+++ src/components/Toplanding/index.js
@@ -6,6 +6,43 @@ import "./index.css";
 const Toplanding =()=>(
     <>
     <div className="bgcontainer">
+        <div className="left">
+            
+            <form id="myForm">
+                <h3 className="formhead">Fill in the details to get a call!</h3>
+
+                <div className="mb">
+                    <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695148422/assignment/person_jkql2i.svg" className="formlogo" />
+                    <input type="text" className="inp"  id="name" placeholder="Full Name" />
+                 
+                </div>
+                <div className="mb">
+                    <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695148422/assignment/call_2_b8tewg.svg" className="formlogo" />
+                    <input type="text" className="inp"  id="name" placeholder="Phone Number" />
+                 
+                </div>
+                <div className="mb">
+                    <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695148422/assignment/distance_mshomw.svg" className="formlogo" />
+                    <input type="text" className="inp"  id="name" placeholder="Location" />
+                 
+                </div>
+                <div className="checkbox">
+                    <input type="checkbox" checked="checked" />
+                    <p className="terms" >By continuing, you agree to our <span className="termsspl">T&C and privacy policy</span></p>
+                </div>
+                <div className="proceed">
+                    <button type="submit" class="btn btn-primary">Proceed</button>
+
+                </div>
+                
+            </form>
+
+        </div>
+
+
+
+
+
 
     </div>
     <div className="accred">
