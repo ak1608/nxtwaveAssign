8244a4bedc561abaf62e3f9f595f774d6ae4c5e2
ak1608 <48668400+ak1608@users.noreply.github.com>
2023-09-20 06:29:49 +0000

    why choose section

--- src/components/Midsec/index.js
+++ src/components/Midsec/index.js
@@ -58,6 +58,63 @@ const Midsec =() =>(
 
         </div>
         <p className="desc">Explore our wide range of tests! We ensure that you don’t miss out on any <br/>chance to keep yourself healthy.</p>
+        <div className="midContainer">
+                <p  className="midTitle">Why <span className="exp">choose</span>us?</p>
+            </div>
+            <div className="testcardcontainer">
+                <div className="testcard">
+                    <p className="testheading">Basic Full Body <br/> checkup</p>
+                    <p className="rev">4.3 <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695189753/assignment/grade_e1v4n1.svg" className="star" /> (75 reviews)</p>
+
+                    <p className="numb">Number of tests - 53</p>
+                    <p className="poffer">Mrp <span className="sploff">10000</span> 8490</p>
+
+                    <div className="bookbutt">
+                    <button className="book">Book now</button>
+                    </div>
+                    
+
+                </div>
+                <div className="testcard">
+                    <p className="testheading">Advance Full body <br/> checkup</p>
+                    <p className="rev">4.7 <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695189753/assignment/grade_e1v4n1.svg" className="star" /> (100 reviews)</p>
+
+                    <p className="numb">Number of tests - 70</p>
+                    <p className="poffer">Mrp <span className="sploff">12000</span> 1000</p>
+
+                    <div className="bookbutt">
+                    <button className="book">Book now</button>
+                    </div>
+                    
+
+                </div>
+                <div className="testcard">
+                    <p className="testheading">Liver Function <br/> Test</p>
+                    <p className="rev">4.3 <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695189753/assignment/grade_e1v4n1.svg" className="star" /> (43 reviews)</p>
+
+                    <p className="numb">Number of tests - 89</p>
+                    <p className="poffer">Mrp <span className="sploff">13,299</span> 11,299</p>
+
+                    <div className="bookbutt">
+                    <button className="book">Book now</button>
+                    </div>
+                    
+
+                </div>
+
+
+
+
+
+
+
+            </div>
+
+
+            <div className="midContainer">
+            <img src="https://res.cloudinary.com/dy3sku9c7/image/upload/v1695144451/assignment/Vector_2_dtgoga.png" className="bluestr" />
+            </div>
+            <p className="desc">We want you and your heart to be healthy so you can live a <br/> happy life!.</p>
     
         
     
